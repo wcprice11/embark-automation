0e8d38721c37fafb207da3bf9e64078eb4d8b7d9
Adam Warnick <warnickad@mtc.byu.edu>
2022-10-19 15:59:41 -0600

    Copied old elements page to new one

--- embark_test_runner.py
+++ embark_test_runner.py
@@ -22,20 +22,5 @@ def run_full_suite():
 
     runner.run(suite)
 
-def run_single_class(testClass):
-    test = unittest.TestLoader().loadTestsFromTestCase(testClass)
-
-    if os.path.exists("report/additional-stylesheet.css"):
-        with open("report/additional-stylesheet.css") as stylesheet:
-            style = stylesheet.read()
-    else:
-        style = ""
-    runner = HTMLTestRunner(log=True, verbosity=1, output='report', title='Test report', report_name='report',
-                            open_in_browser=True, description="HTMLTestReport", tested_by="Christian Price",
-                            add_traceback=True, style=style)
-
-    runner.run(test)
-
 if __name__ == "__main__":
-    # run_full_suite()
-    run_single_class(TestDeleteLanguage)
+    run_full_suite()
